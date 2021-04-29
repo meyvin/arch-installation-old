22f7f6c04d25778f2304f0a8f0e0518447bb7f9a
Meyvin <meyvin@mailbox.org>
2021-04-29 21:49:53 +0200

    Fixed wrong installation path

--- dev-environment.sh
+++ dev-environment.sh
@@ -14,7 +14,7 @@ pikaur -S --noconfirm postman-bin
 pikaur -S --noconfirm sublime-merge
 pikaur -S --noconfirm visual-studio-code-bin
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh.plugins/zsh-syntax-highlighting
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-/bin/echo -e "\e[1;32mManually append zsh-nvm zsh-autosuggestions & zsh-syntax-highlighting to ~/.zshrc plugins()\e[0m"
+/bin/echo -e "\e[1;32mManually append zsh-nvm zsh-autosuggestions zsh-syntax-highlighting to ~/.zshrc plugins()\e[0m"
