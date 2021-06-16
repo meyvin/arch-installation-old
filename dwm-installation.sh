276c3d1f621ad7db7a5300ff275c440fed50b530
Meyvin <meyvin@mailbox.org>
2021-06-16 09:43:45 +0200

    lockscreen dependecies

--- dwm-installation.sh
+++ dwm-installation.sh
@@ -17,7 +17,7 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo pacman -Syu
 
 # Basic Window Manager
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox-developer-edition nitrogen picom wget zsh unrar docker docker-compose flameshot ranger smbclient playerctl file-roller blueman alacritty vlc
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox-developer-edition nitrogen picom wget zsh unrar docker docker-compose flameshot ranger smbclient playerctl file-roller blueman alacritty vlc scrot
 
 # Install the AUR helper Paru
 cd ~
@@ -28,9 +28,6 @@ rm -r ~/paru
 
 # Install personal AUR packages
 paru -S --noconfirm checkupdates+aur
-paru -S --noconfirm dotnet-host-bin
-paru -S --noconfirm dotnet-runtime-bin
-paru -S --noconfirm dotnet-sdk-bin
 paru -S --noconfirm ferdi-bin
 paru -S --noconfirm gitflow-avh
 paru -S --noconfirm intellij-idea-ultimate-edition
@@ -50,6 +47,8 @@ paru -S --noconfirm arc-gtk-theme
 paru -S --noconfirm dunst
 paru -S --noconfirm sxiv
 paru -S --noconfirm pcmanfm
+paru -S --noconfirm i3lock-color
+paru -S --noconfirm xidlehook 
 #paru -S --noconfirm auto-cpufreq
 
 # Flatpak software
