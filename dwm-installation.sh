8f68465b92a1f5f39ba2f422f59a6d6fdd2fcf82
Meyvin <meyvin@mailbox.org>
2021-07-09 20:41:05 +0200

    Changed to wayland

--- dwm-installation.sh
+++ dwm-installation.sh
@@ -14,13 +14,13 @@ sudo firewall-cmd --reload
 
 # Enable Multilib
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo pacman -Syu
+sudo pacman -Syuu
 
 # Basic Window Manager
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
-firefox-developer-edition nitrogen picom wget zsh unrar docker docker-compose
+sudo pacman -S xorg-wayland qt6-wayland firefox-developer-edition nitrogen picom wget zsh unrar docker docker-compose
 flameshot ranger smbclient playerctl file-roller blueman alacritty vlc scrot
-zathura thunderbird libreoffice-fresh libreoffice-fresh-nl
+zathura thunderbird libreoffice-fresh libreoffice-fresh-nl 
+nordic-theme tela-icon-theme
 
 # Install the AUR helper Paru
 cd ~
@@ -40,49 +40,30 @@ paru -S --noconfirm nerd-fonts-complete
 paru -S --noconfirm otf-monaco-powerline-font-git
 paru -S --noconfirm postman-bin
 paru -S --noconfirm siji
-paru -S --noconfirm sublime-merge
 paru -S --noconfirm timeshift
 paru -S --noconfirm timeshift-autosnap
 paru -S --noconfirm ttf-font-awesome
 paru -S --noconfirm visual-studio-code-bin
-paru -S --noconfirm arc-gtk-theme
+paru -S --noconfirm nordic-theme
+paru -S --noconfirm tela-icon-theme
 paru -S --noconfirm dunst
 paru -S --noconfirm sxiv
-paru -S --noconfirm pcmanfm
-paru -S --noconfirm betterlockscreen
-paru -S --noconfirm xidlehook 
-paru -S --noconfirm brave-bin
-paru -S --noconfirm pamixer
 #paru -S --noconfirm auto-cpufreq
 
 # Flatpak software
 sudo flatpak install -y spotify
 
-
 #sudo systemctl enable --now auto-cpufreq
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 
-# Install DWM
-cd ~ && mkdir .dwm
-git clone https://github.com/meyvin/dwm.git ~/.dwm/dwm
-cd ~/.dwm/dwm && sudo make clean install && cd ~
-
-# Install Dmenu
-git clone https://github.com/meyvin/dmenu.git ~/.dwm/dmenu
-cd ~/.dwm/dmenu && sudo make clean install && cd ~
-
-# Install ST
-#git clone https://github.com/meyvin/st.git ~/.dwm/st
-#cd ~/.dwm/st && sudo make clean install && cd ~
-
-# Install DWMBlock
-git clone https://github.com/meyvin/dwmblocks.git ~/.dwm/dwmblocks
-cd ~/.dwm/dwmblocks && sudo make clean install && cd ~
-
 # Install Dotfiles
 git clone https://github.com/meyvin/dotfiles.git ~/Dotfiles
-chmod +x ~/Dotfiles/symlink.sh && ~/Dotfiles/symlink.sh
+cd ~/Dotfiles; chmod +x install.sh; ./install.sh
+
+# Set Gnome theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Nordic'
+gsettings set org.gnome.desktop.interface icon-theme 'Tela-blue-dark'
 
 /bin/echo -e "\e[1;32mStarting DWM  in 5..4..3..2..1..\e[0m"
 sleep 5
