f1a73a49a34837010b69831cf40163363fa3be25
Meyvin <meyvin@mailbox.org>
2021-07-13 11:10:49 +0200

    rewrote dotfiles installation

--- sway-installation.sh
+++ sway-installation.sh
@@ -19,7 +19,7 @@ sudo pacman -Syuu
 # Basic Window Manager
 sudo pacman -S xorg-xwayland firefox-developer-edition wget zsh unrar docker
 docker-compose smbclient playerctl file-roller blueman alacritty vlc zathura
-thunderbird libreoffice-fresh libreoffice-fresh-nl thunar sway swaylock
+zathura-cb zathura-pdf-mupdf thunderbird libreoffice-fresh libreoffice-fresh-nl thunar sway swaylock
 swayidle
 
 # Install personal AUR packages
@@ -53,8 +53,20 @@ sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 
 # Install Dotfiles
-git clone https://github.com/meyvin/dotfiles.git ~/Dotfiles
-cd ~/Dotfiles; chmod +x install.sh; ./install.sh
+git clone --bare https://github.com/meyvin/dotfiles.git $HOME/.dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p $HOME/.dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
 
 # Set Gnome theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Nordic'
